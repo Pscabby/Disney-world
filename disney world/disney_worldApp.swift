d4700659fe7f271b63d21a6c759afd1d5d550aad
Pscabby <pscabby0506@gmail.com>
2022-10-16 14:18:22 +0800

    Initial Commit

---
+++ disney world/disney_worldApp.swift
@@ -0,0 +1,17 @@
+//
+//  disney_worldApp.swift
+//  disney world
+//
+//  Created by Abby on 2022/10/16.
+//
+
+import SwiftUI
+
+@main
+struct disney_worldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
